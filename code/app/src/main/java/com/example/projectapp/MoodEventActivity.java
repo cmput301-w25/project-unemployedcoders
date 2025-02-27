36f1de4318ff81b17526f840bf7781bd08ccda17
Saksham186 <157849487+Saksham186@users.noreply.github.com>
2025-02-27 14:45:08 -0700

    update

--- code/app/src/main/java/com/example/projectapp/MoodEventActivity.java
+++ code/app/src/main/java/com/example/projectapp/MoodEventActivity.java
@@ -1,5 +1,15 @@
 package com.example.projectapp;
 
+import static androidx.core.content.ContextCompat.startActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
