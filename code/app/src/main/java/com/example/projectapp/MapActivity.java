7f9913d1869b1bdb9512c90bf52793747bdf4935
lukemyaremko <lmyaremk@ualberta.ca>
2025-03-19 16:09:58 -0600

    Current location on app

--- code/app/src/main/java/com/example/projectapp/MapActivity.java
+++ code/app/src/main/java/com/example/projectapp/MapActivity.java
@@ -1,10 +1,16 @@
 package com.example.projectapp;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -14,6 +20,9 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private static final int REQUEST_LOCATION_PERMISSION = 2001;
+    private LatLng eventLocation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,5 +62,29 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         googleMap.addMarker(new MarkerOptions()
                 .position(new LatLng(0, 0))
                 .title("Marker"));
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        } else {
+            GeoLocation geoLocation = new GeoLocation(this);
+            geoLocation.fetchFreshLocation(new GeoLocation.OnLocationReceivedListener() {
+                @Override
+                public void onLocationReceived(Location location) {
+                    eventLocation = new LatLng(location.getLatitude(), location.getLongitude());
+                    googleMap.addMarker(new MarkerOptions()
+                            .position(eventLocation)
+                            .title("Your Location"));
+                }
+
+                @Override
+                public void onLocationFailure(String error) {
+                    Toast.makeText(MapActivity.this, error, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
