414270cbd18e514a3a582eb406162a6773fa283a
lukemyaremko <lmyaremk@ualberta.ca>
2025-03-31 16:12:11 -0600

    docs

--- code/app/src/main/java/com/example/projectapp/models/Comment.java
+++ code/app/src/main/java/com/example/projectapp/models/Comment.java
@@ -1,8 +1,21 @@
+// -----------------------------------------------------------------------------
+// File: Comment.java
+// -----------------------------------------------------------------------------
+// This file models a comment for a mood event.
+//
+// Design Pattern: MVC (Model)
+// Outstanding Issues:
+//  N/A
+// -----------------------------------------------------------------------------
+
 package com.example.projectapp.models;
 
 import java.io.Serializable;
 import java.util.Date;
 
+/**
+ * This class models a comment for a mood event.
+ */
 public class Comment implements Serializable {
     private String commenterUid;
     private String commenterUsername; // Optional but useful for display.
@@ -19,35 +32,74 @@ public class Comment implements Serializable {
         this.timestamp = timestamp;
     }
 
-    // Getters and setters
+    /**
+     * Gets commenter uid
+     * @return
+     *      the commenter uid
+     */
     public String getCommenterUid() {
         return commenterUid;
     }
 
+    /**
+     * Sets commenter uid
+     * @param commenterUid
+     *      the uid to set
+     */
     public void setCommenterUid(String commenterUid) {
         this.commenterUid = commenterUid;
     }
 
+    /**
+     * Gets commenter username
+     * @return
+     *      the commenter username
+     */
     public String getCommenterUsername() {
         return commenterUsername;
     }
 
+    /**
+     * Sets commenter username
+     * @param commenterUsername
+     *      commenter username to set
+     */
     public void setCommenterUsername(String commenterUsername) {
         this.commenterUsername = commenterUsername;
     }
 
+    /**
+     * Gets the comment text
+     * @return
+     *      the comment text
+     */
     public String getText() {
         return text;
     }
 
+    /**
+     * sets comment text
+     * @param text
+     *      text to set in comment
+     */
     public void setText(String text) {
         this.text = text;
     }
 
+    /**
+     * gets the timestamp
+     * @return
+     *      the timestamp of the comment
+     */
     public Date getTimestamp() {
         return timestamp;
     }
 
+    /**
+     * Sets the timestamp
+     * @param timestamp
+     *      timestamp to set
+     */
     public void setTimestamp(Date timestamp) {
         this.timestamp = timestamp;
     }
